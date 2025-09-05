1fde2a15017f5721ae6f003cb56e117a1279bdee
Dinihz <lucas87diniz@gmail.com>
2025-09-05 20:46:14 -0300

    feat: configure TypeOrmModule in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +7,21 @@ import { UsersModule } from './users/users.module';
 import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ReservationsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5000,
+      username: 'postgres',
+      password: 'your_password',
+      database: 'meeting_rooms_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ReservationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
