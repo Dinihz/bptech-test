03b8210e73b5bd12108f5126f05d1412e856a3ee
Dinihz <lucas87diniz@gmail.com>
2025-09-06 18:41:08 -0300

    refactor: move TypeOrmModule from app.module.ts to database.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,29 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
     AuthModule,
     UsersModule,
     ReservationsModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
