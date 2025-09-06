05d30e3a42e7f74e298628e5545840a98097fb9f
Dinihz <lucas87diniz@gmail.com>
2025-09-06 13:38:41 -0300

    feat: add ConfigModule and update TypeOrmModule configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,16 +5,19 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5000,
-      username: 'postgres',
-      password: 'your_password',
-      database: 'meeting_rooms_db',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
