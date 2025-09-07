5534c7ea5dfa2f1952c68838fc4c02c5f1bf3c35
Dinihz <lucas87diniz@gmail.com>
2025-09-07 09:51:20 -0300

    feat: use JWT_SECRET from .env in AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        singOptions: { expiresIn: '3h' },
+      }),
+    }),
+  ],
+})
 export class AuthModule {}
