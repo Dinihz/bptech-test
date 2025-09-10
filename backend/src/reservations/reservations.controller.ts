7c2866ad3814d947d2c869d78fd926083bb73ae6
Dinihz <lucas87diniz@gmail.com>
2025-09-10 16:59:21 -0300

    feat(reservations): update and protect routes with AuthGuard in reservations.controller.ts

--- backend/src/reservations/reservations.controller.ts
+++ backend/src/reservations/reservations.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ReservationsService } from './reservations.service';
+import { CreateReservationDto } from './dto/create-reservation.dto';
 
 @Controller('reservations')
-export class ReservationsController {}
+@UseGuards(AuthGuard('jwt'))
+export class ReservationsController {
+  constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Post()
+  create(@Body() createReservationDto: CreateReservationDto, @Request() req) {
+    return this.reservationsService.create(createReservationDto, req.user);
+  }
+}
