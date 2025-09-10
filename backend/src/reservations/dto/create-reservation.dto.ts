817ab27fd6b8e78fadd765d1d5dfdcb7ab7df1d1
Dinihz <lucas87diniz@gmail.com>
2025-09-10 16:39:08 -0300

    feat(reservations): add validation to reservations DTO with class-validator

--- backend/src/reservations/dto/create-reservation.dto.ts
+++ backend/src/reservations/dto/create-reservation.dto.ts
@@ -1,6 +1,19 @@
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDateString()
+  @IsNotEmpty()
   date: Date;
+
+  @IsDateString()
+  @IsNotEmpty()
   startTime: Date;
+
+  @IsDateString()
+  @IsNotEmpty()
   endTime: Date;
+
+  @IsString()
+  @IsNotEmpty()
   roomId: string;
 }
