7e1ddb79ea73cd8bf24fe2ae8dffa0e97fa6e6e2
Dinihz <lucas87diniz@gmail.com>
2025-09-12 16:43:36 -0300

    chore(database): add test migration in migrations/

---
+++ backend/src/migrations/1757704024576-TestInit.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TestInit1757704024576 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
