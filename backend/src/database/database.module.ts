bc192c8ed1fccd42bf1f67abf2687d9d5b1dd1c5
Dinihz <lucas87diniz@gmail.com>
2025-09-12 16:38:53 -0300

    feat(database): update DatabaseModule to run migrations

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -14,8 +15,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: config.get<string>('DB_USER'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: config.get<string>('NODE_ENV') !== 'production',
+        entities: [join(__dirname, '..', '**', '*.entity.{js,ts}')],
+        migrations: [join(__dirname, '..', 'migrations', '*.{js,ts}')],
+        synchronize: false,
       }),
     }),
   ],
