b1f3ca462822fc19aaafc3b4480653c900f541dc
Dinihz <lucas87diniz@gmail.com>
2025-09-13 01:24:57 -0300

    feat(frontend): configure navbar and routes in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,22 @@
-import { BrowserRouter,Routes, Route, Link } from "react-router-dom"
-import { LoginPage } from "./pages/LoginPage"
-import { RegisterPage } from "./pages/RegisterPage"
-import { DashboardPage } from "./pages/DashboardPage"
+import { BrowserRouter,Routes, Route} from 'react-router-dom'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
+import { DashboardPage } from './pages/DashboardPage'
+import { Navbar } from './components/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="bg-gray-800 p-4 text-white flex gap-4">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Cadastro</Link>
-        <Link to="/dashboard">Dashboard (Protegido)</Link>
-      </nav>
+    <Navbar />
 
-      <div className="p-8">
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/" element={<LoginPage /> } />
-        </Routes>
-      </div>
+    <div className='p-8'>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+          <Route path='/' element={<LoginPage />} />
+      </Routes>
+    </div>
     </BrowserRouter>
   );
 }
