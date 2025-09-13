fefcec91de72b1a856324402e0a7dd2267965718
Dinihz <lucas87diniz@gmail.com>
2025-09-13 10:33:08 -0300

    feat(frontend): add route protection for DashboardPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
 import { DashboardPage } from './pages/DashboardPage'
 import { Navbar } from './components/Navbar'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 function App() {
   return (
@@ -13,8 +14,10 @@ function App() {
       <Routes>
         <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/dashboard' element={<DashboardPage />} />
           <Route path='/' element={<LoginPage />} />
+
+          <Route element={<ProtectedRoute />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
       </Routes>
     </div>
     </BrowserRouter>
