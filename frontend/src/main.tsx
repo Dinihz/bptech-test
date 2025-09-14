5e2a3860e4eaec499656ad786fede44f0e954da2
Dinihz <lucas87diniz@gmail.com>
2025-09-14 11:51:54 -0300

    chore(frontend): update Toaster and TanStack configuration in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,20 +4,25 @@ import './index.css';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Toaster
-      position='top-center'
-      toastOptions={{
-        style: {
-          background: '#111827',
-          color: '#fff'
-        },
-      }}
-      />
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Toaster
+        position='top-center'
+        toastOptions={{
+          style: {
+            background: '#111827',
+            color: '#fff'
+          },
+        }}
+        />
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
