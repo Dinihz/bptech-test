6dbc0a93c4da3c68385979be1b3dc24c0d782d71
Dinihz <lucas87diniz@gmail.com>
2025-09-13 23:01:48 -0300

    feat(frontend): add toast configuration in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
+      <Toaster
+      position='top-center'
+      toastOptions={{
+        style: {
+          background: '#111827',
+          color: '#fff'
+        },
+      }}
+      />
       <App />
     </AuthProvider>
   </React.StrictMode>,
