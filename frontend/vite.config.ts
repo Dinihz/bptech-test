e5fe42472b2255764e9c2932c0c25544ac4ed373
Dinihz <lucas87diniz@gmail.com>
2025-09-14 11:42:11 -0300

    chore: add hookform, tanstack and zod in packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// Removed Tailwind Vite plugin because project uses Tailwind v3
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react()
   ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+      clientPort: 5173,
+    },
+  },
 })
