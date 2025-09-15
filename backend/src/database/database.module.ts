3a004d9827f1621d06a2dfb684a6c5477ed815ac
Dinihz <lucas87diniz@gmail.com>
2025-09-15 00:03:32 -0300

    chore(test): update database setup for e2e integration test

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -10,14 +10,17 @@ import { join } from 'path';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST', 'localhost'),
+        host:
+          config.get<string>('NODE_ENV') === 'test'
+            ? 'localhost'
+            : config.get<string>('DB_HOST'),
         port: config.get<number>('DB_PORT', 5432),
         username: config.get<string>('DB_USER'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
         entities: [join(__dirname, '..', '**', '*.entity.{js,ts}')],
-        migrations: [join(__dirname, '..', 'migrations', '*.{js,ts}')],
-        synchronize: false,
+        //migrations: [join(__dirname, '..', 'migrations', '*.{js,ts}')],
+        synchronize: true,
       }),
     }),
   ],
