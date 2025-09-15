49554bb2d29226140bc8f40f1a50fc98ab52bff2
Dinihz <lucas87diniz@gmail.com>
2025-09-15 00:17:57 -0300

    feat(api): add Axios interceptor

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,17 @@
-import axios from 'axios';
+import axios, { type InternalAxiosRequestConfig, type AxiosRequestHeaders } from 'axios';
 
 const api = axios.create({
   baseURL: '/api',
+});
+
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    const headers: AxiosRequestHeaders = (config.headers ?? {}) as AxiosRequestHeaders;
+    headers.Authorization = `Bearer ${token}`;
+    config.headers = headers;
+  }
+  return config;
 });
 
 export default api;
